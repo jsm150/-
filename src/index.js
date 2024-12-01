c6c2055268936235511b8293d7e53dd3db965d60
Jung Sumin <drt00784@naver.com>
2024-12-01 23:10:21 +0900

    Init 홈페이지 디자인의 초안을 작성한다

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// 대한 민국 중심
+const lat = 36.517464
+const lng = 127.817132
+
+var mapContainer = document.querySelector('#map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(lat, lng), // 지도의 중심좌표
+        level: 13 // 지도의 확대 레벨
+    };
+
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption); 
